064a83cddbd3fe28164c3f56f8e103abb0e495cf
wmackowiak <w.mackowiak@wp.pl>
2020-03-02 20:19:21 +0100

    Pierwszy dzien

---
+++ Zajecia01/zad2.py
@@ -0,0 +1,8 @@
+# Zadanie 2
+# Napisz skrypt, ktory pobiera dwie wiadomosci od uzytkownika
+# a nastepnie wyswietla je na ekranie poprzedzone ostrzezeniem "UWAGA: ..."
+
+wart1=input("Podaj wartość")
+wart2=input("Podaj wartość")
+print("UWAGA" + wart1)
+print("UWAGA" + wart2)
---
+++ Zajecia01/zad4.py
@@ -0,0 +1,11 @@
+# Zadanie 4
+# Napisz skrypt, który zapyta użytkownika o trzy liczby całkowite,
+# a następnie pomnóż dwa pierwsze. przed podzieleniem wyniku przez trzecią liczbę. 
+# Wejście i wyjście powinny być zrozumiałe dla użytkownika.
+
+liczba1=int(input("Podaj liczbę 1"))
+liczba2=int(input("Podaj liczbę 2"))
+liczba3=int(input("Podaj liczbę 3"))
+print(liczba1 / liczba2 * liczba3)
+
+
